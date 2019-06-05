998c0d9999f6c4c88f2852334c80114498b2c9d3
Серый Игорь <seryy.i@rozetka.com.ua>
2019-06-05 17:21:57 +0300

    add vendor repo for adding pre-commit

--- src/setup.bat
+++ src/setup.bat
@@ -7,4 +7,6 @@ if exist .git/hooks/pre-commit (
 )
 
 xcopy /s /f /y "vendor\awesomeua\phpcs-pre-commit-hook\src\pre-commit.win" ".git\hooks\pre-commit"
+xcopy /s /f /y "vendor\awesomeua\phpcs-pre-commit-hook\src\pre-commit.win" "vendor\splitter-modules\market.gomer\.git\hooks\pre-commit"
 xcopy /s /f /y "vendor\awesomeua\phpcs-pre-commit-hook\src\pre-commit.ps1" ".git\hooks\pre-commit.ps1"
+xcopy /s /f /y "vendor\awesomeua\phpcs-pre-commit-hook\src\pre-commit.ps1" "vendor\splitter-modules\market.gomer\.git\hooks\pre-commit.ps1"
--- src/setup.sh
+++ src/setup.sh
@@ -11,8 +11,15 @@ if [ ! -d .git/hooks ]; then
   mkdir -p .git/hooks
 fi
 
+if [ ! -d vendor/splitter-modules/market.gomer/.git/hooks ]; then
+  mkdir -p vendor/splitter-modules/market.gomer/.git/hooks
+fi
+
 cp vendor/awesomeua/phpcs-pre-commit-hook/src/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+
+cp vendor/awesomeua/phpcs-pre-commit-hook/src/pre-commit vendor/splitter-modules/market.gomer/.git/hooks/pre-commit
+chmod +x vendor/splitter-modules/market.gomer/.git/hooks/pre-commit
 
 if [ "$PRE_COMMIT_EXISTS" = 0 ];
 then
